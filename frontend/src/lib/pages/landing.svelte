80cc714d658f4956c82beb8b931bc4510b851a88
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-09-21 00:30:55 +0300

    scroll

--- frontend/src/lib/pages/landing.svelte
+++ frontend/src/lib/pages/landing.svelte
@@ -5,69 +5,112 @@
   import {pubKey} from '$lib/stores/signer';
 
   let width;
+
+  const handleWheel = e => {
+    let jump = e.deltaY;
+
+    let element1 = document.getElementById('1');
+    let position1 = element1.getBoundingClientRect();
+    let y1 = position1.top + pageYOffset;
+
+    let element2 = document.getElementById('2');
+    let position2 = element2.getBoundingClientRect();
+    let y2 = position2.top + pageYOffset;
+
+    if(jump === 100){
+      setTimeout(() => window.scrollBy({top: (y2 - y1 - 100), behavior: 'smooth'}), 200);
+    } else {
+      setTimeout(() => window.scrollBy({top: -(y2 - y1 - 100), behavior: 'smooth'}), 200);
+    }
+  };
 </script>
 
-<svelte:window bind:innerWidth={width}/>
+<svelte:window bind:innerWidth={width} on:wheel={handleWheel}/>
 
 <div class="background">
   <div class="circleYellow"/>
+  <div class="circleOrangeTwo"/>
+  
   <div class="circleOrange"/> 
   <div class="circleGreen"/> 
   <div class="circleGreenTwo"/>
-  <div class="circleOrangeTwo"/>
+  
   <div class="circleYellowThree"/>
   <div class="circleGreenThree"/>  
 </div>
 
 <div class="mainBackground">
   <div class="main">
-    <div class="header">
-      <div class="logo flex">
-        <div class="m-c" style="width: 60px; height: 60px; background-color: white; border-radius: 60px;"/>
-        <h2 class="m-c mont" style="margin-left: 1rem; color:white">LOGOTYPE</h2>
+    
+    <div class="firstField" id="0">
+      <div class="header" >
+        <div class="logo flex">
+          <div class="m-c" style="width: 60px; height: 60px; background-color: white; border-radius: 60px;"/>
+          <h2 class="m-c mont" style="margin-left: 1rem; color:white">LOGOTYPE</h2>
+        </div>
+        {#if width >= 800}
+          <div class="navigationBar flex mont">
+            <h3 on:click={() => {document.getElementById('1').scrollIntoView({behavior: "smooth"}); position = 1}}>About</h3>
+            <h3 on:click={() => {document.getElementById('3').scrollIntoView({behavior: "smooth"}); position = 3}}>Explorer</h3>
+            <h3 on:click={() => {document.getElementById('4').scrollIntoView({behavior: "smooth"}); position = 4}}>FAQ</h3>
+          </div>
+        {/if}
+        <h3 class="m-c mont addObj"><ConnectButton/></h3>
       </div>
-      {#if width >= 800}
-        <div class="navigationBar flex mont">
-          <h3>Name tag</h3>
-          <h3>Name tag</h3>
-          <h3>Name tag</h3>
+      {#if width < 800}
+        <div class="navigationBar dopNavigationBar flex mont">
+          <h3>About</h3>
+          <h3>Explorer</h3>
+          <h3>FAQ</h3>
         </div>
       {/if}
-      <h3 class="m-c mont addObj" style=""><ConnectButton/></h3>
-    </div>
-
-    {#if width < 800}
-      <div class="navigationBar dopNavigationBar flex mont">
-        <h3>Name tag</h3>
-        <h3>Name tag</h3>
-        <h3>Name tag</h3>
-      </div>
-    {/if}
-
-    <div class="firstField">
-        <div>
-          <h1 class="cun">SLIMY FROGES</h1>
-          <p class="mont">
-            Я, в своём познании настолько преисполнился, 
-            что я как будто бы уже 100 триллионов миллиардов лет, <nobr style="font-weight: 600;">понимаешь</nobr>?
+      <div class="firstBody">
+        <div style="margin-top: 15vh;">
+          <h1 class="cun text-sup-big">NUKY FROGS</h1>
+          <p class="mont text-normal">
+            There are <nobr style="font-weight: 600;">10 000</nobr> rarest individuals whole around the Solana metauniverse, that should be
+            caught and held by professionals.
           </p>
         </div>
         <img style="height: 60vh; margin-top: 2rem; border-radius: 4rem;" src="https://preview.redd.it/u0ti5q49c8e71.jpg?width=1024&format=pjpg&auto=webp&s=2c5f241ff362dd36585ccb43dc8fc82d85e25f30" alt="">
+      </div>
     </div>
-    <div class="SecondField" style="height: 100vh;">
-      
+
+    <div class="about" id="1">
+      <img style="height: 60vh; margin-top: 20vh; border-radius: 4rem;" src="https://preview.redd.it/u0ti5q49c8e71.jpg?width=1024&format=pjpg&auto=webp&s=2c5f241ff362dd36585ccb43dc8fc82d85e25f30" alt="">
+      <div>
+        <div style="text-align: right;">
+          <h1 class="cun text-big" style="margin-top: 25vh; margin-bottom: 5vh;">Extremely poisonous</h1>
+          <p class="mont text-normal">● Poisonous skin</p>
+          <p class="mont text-normal">● Radiation exposure</p>
+          <p class="mont text-normal">● Uniqueness</p>
+          <p class="mont text-normal">● Great scientific value</p>
+          <p class="mont text-normal">● Mutations</p>
+        </div>
+        <p class="mont text-normal" style="text-align: right; width: 42rem; margin-top: 5vh;">
+          Each Nuky Frog has its own attributes, which were accidentally created by the power of nature.
+          Our scientific community has rated them from “common” to “extreme,” and each rank has its own rating.
+        </p>
+      </div>
     </div>
-    <div class="SecondField" style="height: 100vh;">
-      
+
+    <div class="minting flex" id="2" style="height: 100vh;">
+      <p class="cun text-sup-big m-c" style="text-align: center; width:100%">Get a frog!</p>
     </div>
-    <div class="SecondField" style="height: 100vh;">
-      
+
+    <div class="explorer flex" id="3" style="height: 100vh;">
+      <p class="cun text-sup-big m-c" style="text-align: center; width:100%">Explorer!</p>
+    </div>
+
+    <div class="FAQ flex" id="4" style="height: 100vh;">
+      <p class="cun text-sup-big m-c" style="text-align: center; width:100%">FAQ!</p>
     </div>
 
   </div>
 </div>
 
 <style>
+/* Тут шарики лежат - нужно будет всеже обобщить */
   .background{
     z-index: 1;
     background: rgba(255, 255, 255, 0.01);
@@ -104,7 +147,9 @@
     width: 25vw;
     height: 50vw;
     border-radius: 50vw 0 0 50vw;
-    left: 75vw;
+    left: 0;
+    right: 0;
+    margin-left: auto;
     top: 125vw;
     background-color: #F9CA24;
   }
@@ -113,7 +158,11 @@
     width: 25vw;
     height: 50vw;
     border-radius: 50vw 0 0 50vw;
-    left: 75vw;
+    
+    left: 0;
+    right: 0;
+    margin-left: auto;
+
     top: 25vw;
     background-color: rgb(238, 255, 0);
   }
@@ -122,7 +171,7 @@
     width: 50vh;
     height: 50vh;
     border-radius: 0vh 100vh 0vh 0vh;
-    top: 350vh;
+    top: 450vh;
     left: 0vh;
     background-color: #6AB04C;
   }
@@ -131,17 +180,17 @@
     width: 40vh;
     height: 40vh;
     border-radius: 0vh 100vh 0vh 0vh;
-    top: 360vh;
+    top: 460vh;
     left: 30vh;
     background-color: rgb(238, 255, 0);
   }
-
+/*  */
   .mainBackground{
     position: absolute;
     z-index: 3;
     backdrop-filter: blur(150px);
-    width: 100vw;
-    height: 400vh;
+    width: 100%;
+    height: 500vh;
   }
   .main{
     max-width: 1440px;
@@ -177,27 +226,25 @@
   .firstField{
     margin-left: 2rem;
     margin-right: 2rem;
+  }
+  .firstField .firstBody{
     height: 85vh;
     display: flex;
     justify-content: space-between;
-  }
-  .firstField div{
-    /* margin-top: auto; */
-    margin-top: 15vh;
-    /* margin-bottom: auto; */
-  }
-  .firstField h1{
-    color: azure;
-    font-size: 75px;
+    
   }
   .firstField p{
-    color: azure;
-    font-size: 24px;
-    line-height: 1.8rem;
-    margin-top: -1.6rem;
+    margin-top: -1.8rem;
     width: 26rem;
   }
   
+  .about{
+    display: flex;
+    height: 100vh;
+    margin-left: 2rem;
+    margin-right: 2rem; 
+    justify-content: space-between;
+  }
 
   @media (max-width:950px){
     .navigationBar{
