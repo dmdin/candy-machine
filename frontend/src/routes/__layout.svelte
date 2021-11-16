a57c1914de3a9ea932a1eb88037eea45a67907d9
Din Dmitriy <dmitriy1d01@gmail.com>
2021-11-16 12:57:00 +0300

    Added monitoring

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,8 +1,24 @@
 <script>
-	import '../app.css';
-	import { Buffer } from 'buffer';
+  import '../app.css';
+  import {Buffer} from 'buffer';
+  global.Buffer = Buffer;
 
-	global.Buffer = Buffer;
+  import * as Sentry from "@sentry/browser";
+  import {Integrations} from "@sentry/tracing";
+  import { CaptureConsole } from '@sentry/integrations';
+
+  Sentry.init({
+    dsn: "https://41e150efdd374340a94fb862f21a2772@o1070274.ingest.sentry.io/6066010",
+    integrations: [
+      new Integrations.BrowserTracing(),
+      new CaptureConsole({levels: ['error', 'info',]}),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
 </script>
-
-<slot />
+<slot/>
